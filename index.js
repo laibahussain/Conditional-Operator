72e2c17e089502a31c9c74fea6930cea9defa0fa
laibahussain <laibah42@gmail.com>
2024-10-14 15:25:00 +0500

    If else practice

---
+++ index.js
@@ -0,0 +1,35 @@
+let num1 = parseInt(prompt("enter num 1"));
+let num2 = parseInt(prompt("enter num 2"));
+alert( , num1 + num2);
+console.log("hey" , num1 + num2);
+
+// let day = parseInt(prompt("enter your birth day"));
+// let month = parseInt(prompt("enter your birth month"));
+// let year = parseInt(prompt("enter your birth year"));
+// if((day > 1 && day < 32) && (month > 1 && month < 12) && (year > 2002 && year < 2023)){
+//   console.log("your birthday is" , date, month, year)
+//   else{
+//     console.log("invalid date")
+//   }
+// }
+
+
+// let day = parseInt(prompt("enter your birth day"));
+// let month = parseInt(prompt("enter your birth month"));
+// let year = parseInt(prompt("enter your birth year"));
+// if((day > 1 && day < 32) && (month > 1 && month < 12) && (year > 2002 && year < 2023)){
+//   console.log("your addmission date is" , date, month, year)
+// }  else{
+//     console.log("invalid date")
+//   } 
+
+// let oldemail = "laibah42@gmail.com";
+// let pass = "abc123";
+// let email = prompt("enter your email address");
+// let password = prompt("enter your password");
+// if((email === oldemail && password === pass)){
+//   console.log("Welcome to Allied Bank Limited ABL")
+// }  else{
+//     console.log("invalid")
+//   }
+
